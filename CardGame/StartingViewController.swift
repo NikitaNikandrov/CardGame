d71fbe60893d61017fc248704ac1d89eac399ed0
NikitaNikandrov <nnikandroff@yandex.ru>
2021-07-19 20:37:01 +0300

    created outlets, actions in start view controller

--- CardGame/StartingViewController.swift
+++ CardGame/StartingViewController.swift
@@ -9,9 +9,22 @@ import UIKit
 
 class StartingViewController: UIViewController {
 
+    //Outlets
+    @IBOutlet weak var difficultLabel: UILabel!
+    @IBOutlet weak var easyButton: UIButton!
+    @IBOutlet weak var mediumButton: UIButton!
+    
+    //Actions
+    @IBAction func easyButtonIsPressed(_ sender: Any) {
+    }
+    
+    @IBAction func mediumButtonIsPressed(_ sender: Any) {
+    }
+    
+    //Live cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 }
 
