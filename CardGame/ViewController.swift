5931406d012efc38138d7304d391e9513f13fcaa
NikitaNikandrov <nnikandroff@yandex.ru>
2021-07-19 20:16:06 +0300

    Initial Commit

---
+++ CardGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CardGame
+//
+//  Created by Никита on 19.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
