ad5641f76b0efff1209476790132783143a0c7e9
Nikita Nikandrov <nnikandroff@yandex.ru>
2025-01-01 20:29:29 +0300

    Initial Commit

---
+++ CardGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CardGame
+//
+//  Created by Nikita Nikandrov on 01.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
