b64c5e63ccd3edcbf01996d6e81c9a15b5b66aca
Nikita Nikandrov <nnikandroff@yandex.ru>
2025-01-01 23:44:52 +0300

    deleted storyboard and scenedelegate

--- CardGame/View/Controllers/StartViewController.swift
+++ CardGame/View/Controllers/StartViewController.swift
@@ -11,7 +11,7 @@ class StartViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .green
     }
 
 
