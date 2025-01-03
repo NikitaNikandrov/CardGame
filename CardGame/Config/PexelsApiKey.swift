20a39a89750b221127638764e8a3485656431f96
Nikita Nikandrov <nnikandroff@yandex.ru>
2025-01-03 19:31:25 +0300

    fixed PexelsApiKey

--- CardGame/Config/PexelsApiKey.swift
+++ CardGame/Config/PexelsApiKey.swift
@@ -1,1 +1,12 @@
+//
+//  PexelsAPIKey.swift
+//  CardGame
+//
+//  Created by Nikita Nikandrov on 02.01.2025.
+//
 
+import Foundation
+
+struct PexelsAPIKey {
+    static let key = "ksbdoCLD1ClBPvcp3x1zidFRLA2JWKyteYS25YPhjgB5UexGCslqekB0"
+}
